b91ea4e9114f0fae004975a79cb7fdea55fd4c82
shaktibiplabDev <shaktibiplab@gmail.com>
2024-10-27 20:48:25 +0000

    first commit

---
+++ software/bernie/example/client/model/entity/ExampleEntityModel.java
@@ -0,0 +1,44 @@
+package software.bernie.example.client.model.entity;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.GeckoLib;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
+
+public class ExampleEntityModel extends AnimatedGeoModel
+{
+	@Override
+	public ResourceLocation getAnimationFileLocation(Object entity)
+	{
+		return new ResourceLocation(GeckoLib.ModID, "animations/magma_spider.animation.json");
+	}
+
+	@Override
+	public ResourceLocation getModelLocation(Object entity)
+	{
+		return new ResourceLocation(GeckoLib.ModID, "geo/magma_spider.geo.json");
+	}
+
+	@Override
+	public ResourceLocation getTextureLocation(Object entity)
+	{
+		return new ResourceLocation(GeckoLib.ModID, "textures/model/entity/magma_spider.png");
+	}
+
+
+	@Override
+	public void setLivingAnimations(IAnimatable entity, Integer uniqueID, AnimationEvent customPredicate)
+	{
+		super.setLivingAnimations(entity, uniqueID, customPredicate);
+		IBone head = this.getAnimationProcessor().getBone("head");
+
+		//EntityLivingBase entityIn = (EntityLivingBase) entity;
+		//EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+		//head.setRotationX(extraData.headPitch * ((float)Math.PI / 180F));
+		//head.setRotationY(extraData.netHeadYaw * ((float)Math.PI / 180F));
+	}
+}
---
+++ software/bernie/geckolib3/file/AnimationFile.java
@@ -0,0 +1,26 @@
+package software.bernie.geckolib3.file;
+
+import software.bernie.geckolib3.core.builder.Animation;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class AnimationFile
+{
+	private HashMap<String, Animation> animations = new HashMap<>();
+
+	public Animation getAnimation(String name)
+	{
+		return animations.get(name);
+	}
+
+	public void putAnimation(String name, Animation animation)
+	{
+		this.animations.put(name, animation);
+	}
+
+	public Collection<Animation> getAllAnimations()
+	{
+		return this.animations.values();
+	}
+}
